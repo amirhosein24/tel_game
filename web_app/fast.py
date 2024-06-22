8402b6cea3b32de3d900c9511e5805e2665f5242
garshasp <garshaspx@gmail.com>
2024-06-22 15:26:30 +0330

    update

--- web_app/fast.py
+++ web_app/fast.py
@@ -8,6 +8,8 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Room manager to handle user connections
+
+
 class RoomManager:
     def __init__(self):
         self.active_rooms = {}
@@ -18,13 +20,11 @@ class RoomManager:
             self.active_rooms[room_id] = {}
         self.active_rooms[room_id][user_id] = websocket
 
-
     def disconnect(self, room_id: str, user_id: str):
         if room_id in self.active_rooms:
             self.active_rooms[room_id].pop(user_id, None)
             if not self.active_rooms[room_id]:
                 self.active_rooms.pop(room_id)
-
 
     async def broadcast(self, room_id: str, message: str, sender_id: str):
         if room_id in self.active_rooms:
@@ -32,12 +32,15 @@ class RoomManager:
                 if user_id != sender_id:
                     await websocket.send_text(message)
 
+
 room_manager = RoomManager()
+
 
 @app.get("/home/{room_id}/{user_id}", response_class=HTMLResponse)
 async def read_index(room_id: str, user_id: str):
     index_file = Path(__file__).parent / "templates" / "index.html"
     return index_file.read_text()
+
 
 @app.websocket("/ws/{room_id}/{user_id}")
 async def websocket_endpoint(websocket: WebSocket, room_id: str, user_id: str):
@@ -48,6 +51,26 @@ async def websocket_endpoint(websocket: WebSocket, room_id: str, user_id: str):
             await room_manager.broadcast(room_id, data, user_id)
     except WebSocketDisconnect:
         room_manager.disconnect(room_id, user_id)
+
+
+# import asyncio
+# from fastapi import WebSocketDisconnect
+
+# # ...
+
+# async def websocket_endpoint(websocket: WebSocket, room_id: str, user_id: str):
+#     await room_manager.connect(websocket, room_id, user_id)
+#     try:
+#         while True:
+#             # Set a timeout of 5 seconds to wait for data from the client
+#             data = await asyncio.wait_for(websocket.receive_text(), timeout=5)
+#             await room_manager.broadcast(room_id, data, user_id)
+#     except WebSocketDisconnect:
+#         room_manager.disconnect(room_id, user_id)
+#     except asyncio.TimeoutError:
+#         # Handle the timeout error here
+#         pass
+
 
 if __name__ == "__main__":
     import uvicorn
